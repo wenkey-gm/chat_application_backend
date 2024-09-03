f62c7f02c1609057b74389a15829a9aa4b4bd197
wenkey <g.m.venkateshmuni@gmail.com>
2024-09-03 15:01:02 +0200

    feat: create database connection

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -3,7 +3,7 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
+DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
