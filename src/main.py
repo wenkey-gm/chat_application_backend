732f1da4f97097bcf1df1fa481664e36b677f178
wenkey <g.m.venkateshmuni@gmail.com>
2024-09-04 16:20:13 +0200

    refactor: update create_tables and drop_tables functionality

--- src/main.py
+++ src/main.py
@@ -3,30 +3,32 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.controller.user_controller import user_router
 from src.config.database import Database
-from src.utils.init_tables import InitializeTables
+from src.utils.init_tables import create_tables, drop_tables
 from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
-async def lifespan() -> None:
-    db = Database.initialize_database()
-    tables = InitializeTables(database=db)
-    tables.drop_tables()
+async def lifespan(app: FastAPI):
+    Database.initialize_database()
+    create_tables()
+    yield
+    Database.session.close()
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+
 
 origins = [
-    "http://localhost:3000", 
-    "http://127.0.0.1:3000", 
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
-    allow_credentials=True,  
-    allow_methods=["*"], 
-    allow_headers=["*"], 
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user_router)
