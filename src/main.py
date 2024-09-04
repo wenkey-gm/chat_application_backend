3b144ac9339a75b88436dcbccc18c6be1aa63d61
wenkey <g.m.venkateshmuni@gmail.com>
2024-09-04 15:14:06 +0200

    feat: Enable cors for localhost

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from src.controller.user_controller import user_router
 from src.config.database import Database
 from src.utils.init_tables import InitializeTables
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -14,5 +15,18 @@ async def lifespan() -> None:
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 app.include_router(user_router)
