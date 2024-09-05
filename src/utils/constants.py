dfe45f4008d7ba72ee119648bb485b519e86501c
wenkey <g.m.venkateshmuni@gmail.com>
2024-09-05 13:49:29 +0200

    feat: Add test cases for API

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,7 +6,9 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
+TEST_DB_NAME = os.getenv("TEST_DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+TEST_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{TEST_DB_NAME}"
